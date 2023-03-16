03ae5e3fb6a68cec89d39456204225a15486ce18
proyect_icaro <josuevalenzuela87@gmail.com>
2023-03-16 07:13:03 -0300

    Upload import in App.jsx and delete CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { CartContainer } from './components/CartContainer/CartContainer'
 import Navbar from './components/Header/Navbar'
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
@@ -18,7 +17,7 @@ function App() {
       <Route path='/' element={<ItemListContainer />}/>
       <Route path="/productos/:tipoProducto" element={<ItemListContainer />}/>
       <Route path="/item/:productId" element={<ItemDetailContainer/>}/>
-      <Route path="/cart" element={<CartContainer/>}/>
+      
       </Routes>
     
     </CartDetailsProvider>
